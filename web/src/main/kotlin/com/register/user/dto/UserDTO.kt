96135a8c5754880dd2caf030d607f182e23de4f8
Adriano Ribeiro <adrianolucianoribeiro@gmail.com>
2019-06-16 16:47:28 -0300

    Adding email validation

--- web/src/main/kotlin/com/register/user/dto/UserDTO.kt
+++ web/src/main/kotlin/com/register/user/dto/UserDTO.kt
@@ -1,11 +1,16 @@
 package com.register.user.dto
 
 import com.register.entities.User
+import org.hibernate.validator.constraints.Email
 
 
 data class UserDTO (
+
     val name: String = "",
+
+    @field:Email(message = "Invalid email format")
     val email: String = ""
+
 ) {
     fun toEntity() = User(name = name, email = email)
 }
